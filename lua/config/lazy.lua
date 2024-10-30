b7684102f5abbf991650166418a03386acbc7117
Katherine Peng <katherine.j.peng@gmail.com>
2024-10-30 15:38:22 -0400

    disable virtual error messages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -78,3 +78,11 @@ vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
+  end,
+})
